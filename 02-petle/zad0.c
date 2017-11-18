52e7785aa0d9b699318b860929c21f84f9c925f3
Łukasz Dymura <ldymura@linux-p110-1.inf.ug.edu.pl>
2017-11-18 10:07:20 +0100

    zad0

---
+++ 02-petle/zad0.c
@@ -0,0 +1,14 @@
+//Jakie wartości wyświetli poniższy program://
+
+#include <stdio.h>
+int main() {
+    int y, x=1, wynik = 0;
+    while (x<=10) {
+        y = x*x;
+        printf("%d\n", y);
+        wynik += y;
+        ++x;
+    }
+    printf("wynik = %d\n", wynik);
+    return 0;
+}
